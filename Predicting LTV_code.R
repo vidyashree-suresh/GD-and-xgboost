f1028998c8b67d72457b88dd5bb6da29bd01bcc0
Vidya shree S <55318342+vidyashree-suresh@users.noreply.github.com>
2023-11-13 19:08:25 -0500

    Update Predicting LTV_code.R
    Updated the comments and library

--- Predicting LTV_code.R
+++ Predicting LTV_code.R
@@ -1,6 +1,6 @@
 ##--------------Predicting the LTV of first year customers------------------------##
 
-##Author:Vidya shree Suresh
+##Author:Vidya shree 
 ##Date : 7/14/2018
 
 ##-------------------Required packages---------------------------##
@@ -13,12 +13,13 @@ library(MASS)
 library(MLmetrics)
 install.packages("rfUtilities")
 library(rfUtilities)
+library(xgboost)
 ##-----------------------Reading and understanding dataset--------------------------##
 
 
 
-train_dataset<-read_excel('C:/Users/vidya.suresh/Documents/Personal/Lifetime_value.xlsx')
-test_dataset<-read_excel('C:/Users/vidya.suresh/Documents/Personal/LTV_ test_set.xlsx')
+train_dataset<-read_excel('./Lifetime_value.xlsx')
+test_dataset<-read_excel('./LTV_ test_set.xlsx')
 
 dataset<-rbind(train_dataset,test_dataset)
 
@@ -33,7 +34,7 @@ str(dataset)
 ## Created a booking month column in excel converting that to a factor
 dataset$`Booking_month`<-as.factor(dataset$`Booking_month`)
 
-##Converting international flag to a factor
+##Converting the international flag to a factor
 
 dataset$international<-as.factor(dataset$international)
 
@@ -44,7 +45,7 @@ dataset_cf<-dataset_cf_all[1:441,]
 
 
 
-## understanding descriptive statistics in r
+## Understanding descriptive statistics in r
 summary(dataset_cf)
 
 
@@ -69,10 +70,9 @@ rcor<-rcorr(as.matrix(dataset_f[,c(3,5,6)]),type="pearson")
 ###---------------Selection of variables----------------------------------------##
 
 
-rf<-randomForest(ltv~.,data=dataset_cf[,-c(1,6)],importance=T,ntree=1000
-)
+rf<-randomForest(ltv~.,data=dataset_cf[,-c(1,6)],importance=T,ntree=1000)
 
-## Checking for variable importance,lenght of stay followed by booking country are the
+## Checking for variable importance, length of stay followed by booking country are the
 ##two most important variables for prediction
 
 varImpPlot(rf)
@@ -123,8 +123,37 @@ plot(rf.cv, stat = "mae")
 
 ##-----Reading test set for prediction---------------------##
 
-
 test.pred<-predict(rf.model, dataset_cf_all[442:460,-c(1,6,8)])
 test.pred
 
+##------
+
 write.csv(test.pred,"results_ltv_2.csv",row.names = F)
+
+
+#--------Creating XGBoost Model--------------#
+
+
+# Prepare the data for XGBoost
+X_train <- as.matrix(train[, c("length_of_stay", "commission")])
+y_train <- train$ltv
+X_test <- as.matrix(test[, c("length_of_stay", "commission")])
+y_test <- test$ltv
+
+# Train an XGBoost model
+xgb<- xgboost(
+  data = X_train,
+  label = y_train,
+  nrounds = 100,  
+  objective = "reg:squarederror" 
+)
+
+# Objective for the model is squarederror
+
+# Make predictions on the test set
+y_pred <- predict(xgb, as.matrix(X_test))
+
+# Evaluate the model (you can use different metrics depending on your task)
+rmse <- sqrt(mean((y_pred - y_test)^2))
+
+
